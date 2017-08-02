059a24c6be9c47d7a0e95a26ce9da1a700333e88
Israel De Castro <fulatino203@gmail.com>
2017-08-02 16:15:01 -0400

    Initial Commit

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+  appName: 'blastermusic',
+  apiKey: '05453262c8baf0522f94026cf130e946',
+  secret: '4f9b6b9f112c30dd95d1a69e31035c00',
+  registeredTo: 'BlasTer203'
+}
---
+++ src/components/Artist.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  li.col-12.col-md-3
+    h2: a(:href="artist.url" target="_blank") {{ artist.name }}
+    img(:src="artist.image[2]['#text']")
+</template>
+
+<script>
+
+export default {
+  name: 'artist',
+  props: ['artist']
+}
+</script>
+
+<style lang="stylus" scoped>
+  li
+    display inline-block
+    margin 10px
+</style>
